679a6f891ddf4039140f86bf043e43f14044abea
JosephVC <josephvcardenas@gmail.com>
2020-03-21 15:23:56 -0700

    pdf_page_to_image actually works

--- pdf_page_to_image.py
+++ pdf_page_to_image.py
@@ -6,15 +6,20 @@ from pdf2image import convert_from_path
 
 
 # construct the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--pdf", required=True, 
+    help="path to input pdf to be split into images")
+args = vars(ap.parse_args())
+
 
 # produce a list of images from the pdf
 # specifically, these are PIL image files
-images = convert_from_path('[PATH ARGS GO HERE]')
+images = convert_from_path(args["pdf"])
 
 # now we just hop through the above list of images
 # save and output with the title 'output[i].jpg', 
 # specifying the output format as JPEG
-
+i = 1
 for image in images:
    image.save('output' + str(i) + '.jpg', 'JPEG')
    i +=1
