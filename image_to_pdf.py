b32c7be5c6d5d6bbeaa705c6c0b8f04e93cbfe32
JosephVC <josephvcardenas@gmail.com>
2020-03-28 12:51:15 -0700

    updated image_to_pdf

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -3,8 +3,31 @@ This portion of the toolchain will take the OCR's images and recombine them
 - in order - to  make a pdf that is now OCR'd
 """
 
-# import os 
-from pdfrw import PdfWriter
-y = PdfWriter()
-y.addpage(x.pages[0])
-y.write('result.pdf')
+import img2pdf
+from PIL import Image
+import os
+
+# store image and pdf path
+# TODO: update this to take arguments rather than hard-coded paths
+# TODO: update to iterate through a directory of OCR'd images
+img_path = "OCRd_images/Output1.png"
+pdf_path = "OCRd_pdfs/Output1.pdf"
+
+# open image
+image = Image.open(img_path)
+
+#converting into chunks of bytes 
+pdf_bytes = img2pdf.convert(image.filename)
+
+#opening or creating pdf file
+file = open(pdf_path, "wb")
+
+file.write(pdf_bytes)
+
+# close the image and pdf file
+image.close()
+file.close()
+
+# output to signal success
+print("Image -> pdf conversion successful")
+
